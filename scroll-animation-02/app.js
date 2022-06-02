0b7871dd58cf1fe4899999affe4dde2d6f67e94a
Salman Ahmed <salmanahmedx@hotmail.com>
2022-06-03 00:39:50 +0600

    scroll project 02 - final commit

--- scroll-animation-02/app.js
+++ scroll-animation-02/app.js
@@ -12,3 +12,31 @@ const dates = [
   "1792 | AJD",
 ]
 
+ScrollTrigger.create({
+  trigger: ".sections",
+  pin: ".tooltip",
+  start: "top 40%",
+  end: "bottom center",
+  // markers: true
+})
+
+const sections = document.querySelectorAll(".section");
+
+sections.forEach((section, index) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: "top 40%",
+    end: "bottom center",
+    markers: true,
+    onEnter: () => {
+      gsap.set(".tooltip-img", { attr: { src: src[index] } })
+      gsap.set(".tooltip p", { innerText: dates[index] })
+    },
+    onEnterBack: () => {
+      gsap.set(".tooltip-img", { attr: { src: src[index] } })
+      gsap.set(".tooltip p", { innerText: dates[index] })
+    }
+  })
+})
+
+
