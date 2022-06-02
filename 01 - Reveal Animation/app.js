14e187de94db779cc4ed11ecb1c2006d74b2d619
Salman Ahmed <salmanahmedx@hotmail.com>
2022-06-02 08:28:27 +0600

    Reveal Animation - Final Commit

--- 01 - Reveal Animation/app.js
+++ 01 - Reveal Animation/app.js
@@ -1,3 +1,8 @@
 const TL = gsap.timeline()
 
-TL.fromTo(".nav-container a", { autoAlpha: 0, y: "-50px" }, { autoAlpha: 1, y: "0px", duration: .7 })
+TL
+    .fromTo(".nav-container a", { autoAlpha: 0, y: "-50px" }, { autoAlpha: 1, y: "0px", duration: .75 })
+    .fromTo(".home-content h1", { autoAlpha: 0, y: "-50px" }, { autoAlpha: 1, y: "0px", duration: .75 }, ">")
+    .to(".middle-line", { height: "100px" })
+    .fromTo(".home-content img", { y: "-20px" }, { y: "0px", autoAlpha: 1, duration: .75 }, ">")
+    .to(".home-content button", { autoAlpha: 1 })
