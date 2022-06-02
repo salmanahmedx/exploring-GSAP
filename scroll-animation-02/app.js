0d4e7940b4cfddcb6c776b6708a6d15649b72412
Salman Ahmed <salmanahmedx@hotmail.com>
2022-06-03 00:21:05 +0600

    scroll animation 02 - initial commit

---
+++ scroll-animation-02/app.js
@@ -0,0 +1,14 @@
+const src = [
+  "resources/spartan.svg",
+  "resources/castle.svg",
+  "resources/da-vinci.svg",
+  "resources/airplane.svg",
+];
+
+const dates = [
+  "-3300 | 476",
+  "476 | 1492",
+  "1492 | 1792",
+  "1792 | AJD",
+]
+
